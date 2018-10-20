a1edb09da3427ccab726d2846b197af0362e944a
Adil Gondal <agondal@stuy.edu>
2018-10-19 22:27:09 -0400

    adding song

--- tunes.c
+++ tunes.c
@@ -1,1 +1,18 @@
 #include "tunes.h"
+
+int indexOf(char * artist){
+  int i=artist[0];
+  if (artist[0]>=65 && artist[0]<=90){
+    i+=32;
+  }
+  if (i>=97 && i<=122){
+    i-=97;
+    return i;
+  }
+  return 26;
+}
+void add_song(char * name,char *artist){
+  int i=indexOf(artist);
+  struct song_node * song = make_song(name,artist);
+   
+}
--- tunes.h
+++ tunes.h
@@ -1,3 +1,9 @@
 #include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct library{struct song_node * tables[27];};
+
+struct library * add_song(struct library * l, char * name,char *artist);
+int indexOf(char * art);
