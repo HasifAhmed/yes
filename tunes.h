55228c8b8e5f0a0037fd5d20795a07abb04b1e12
Adil Gondal <agondal@stuy.edu>
2018-10-19 23:42:06 -0400

    all needed functions

--- tunes.h
+++ tunes.h
@@ -9,3 +9,11 @@ struct library{struct song_node * tables[27];};
 
 struct library * add_song(struct library * l, char * name,char *artist);
 int indexOf(char * art);
+struct song_node * find_song(char * artist, char * song);
+struct song_node *  find_artist(char *artist);
+void print_letter(char l);
+void print_artist(char * artist);
+void print_library();
+void  delete_song(char *name, char *artist);
+void clear_library();
+void shuffle_library();
